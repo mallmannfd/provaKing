04b5743167911b1170c2da59cd04768d8b32f6a3
Guilherme Mallmann <guilherme.costa@kinghost.com.br>
2018-07-04 17:58:13 -0300

    Rodape adicionado

--- tests/RodapeTest.php
+++ tests/RodapeTest.php
@@ -1,4 +1,7 @@
 <?php
+
+use Unipago\tests\BaseTestCase;
+use Unipago\model\Rodape;
 /**
  * Created by PhpStorm.
  * User: mallmann
@@ -6,7 +9,21 @@
  * Time: 17:35
  */
 
-class RodapeTest
+class RodapeTest extends BaseTestCase
 {
+    /**
+     * @var Rodape
+     */
+    protected $rodape;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->rodape = new Rodape($this->linhaRodape);
+    }
+
+    public function testGetValorTotal()
+    {
+        $this->assertEquals(14112.49, $this->rodape->getValorTotal(), 'Valor inconsistente');
+    }
 }
