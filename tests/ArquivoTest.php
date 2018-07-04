0f15ecc1b94eaa3e29d235f37678507eb9399836
Guilherme Mallmann <guilherme.costa@kinghost.com.br>
2018-07-04 19:15:54 -0300

    Terminando o testes do modelo Arquivo

--- tests/ArquivoTest.php
+++ tests/ArquivoTest.php
@@ -1,4 +1,11 @@
 <?php
+
+use Unipago\model\Arquivo;
+use Unipago\model\Cabecalho;
+use Unipago\model\Rodape;
+use Unipago\model\Titulo;
+use Unipago\tests\BaseTestCase;
+
 /**
  * Created by PhpStorm.
  * User: mallmann
@@ -6,7 +13,67 @@
  * Time: 18:34
  */
 
-class ArquivoTest
+class ArquivoTest extends BaseTestCase
 {
+    /**
+     * @var Arquivo
+     */
+    protected $arquivo;
+
+    /**
+     * @var Cabecalho
+     */
+    protected $cabecalho;
+
+    protected $corpo;
+
+    /**
+     * @var Rodape
+     */
+    protected $rodape;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->cabecalho = new Cabecalho($this->linhaCabecalho);
+        $this->corpo = new Titulo($this->linhaCorpo);
+        $this->rodape = new Rodape($this->linhaRodape);
+
+        $arquivo = $this->linhaCabecalho . "\n";
+        $arquivo .= $this->linhaCorpo . "\n";
+        $arquivo .= $this->linhaRodape;
+
+        $this->arquivo = new Arquivo($arquivo);
+    }
+
+    public function testGetCabecalho()
+    {
+        $this->assertEquals($this->cabecalho, $this->arquivo->getCabecalho(), 'Cabeçalhos inconsistentes');
+    }
+
+    public function testGetCorpo()
+    {
+        $this->assertEquals($this->corpo, $this->arquivo->getCorpo()[0], 'Títulos inconsistentes');
+    }
+
+    public function testGetRodape()
+    {
+        $this->assertEquals($this->rodape, $this->arquivo->getRodape(), 'Rodape inconsistente');
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedException Arquivo não é referente a empresa correta.
+     */
+    public function testDeveGerarExceptionComEmpresaIncorreta()
+    {
+        $this->arquivo->getCabecalho()->setEmpresa('teste');
+        $this->arquivo->validaEmpresa();
+    }
+
+    public function testValidaImportacao()
+    {
+        $this->assertEquals(number_format($this->rodape->getValorTotal(), 2), number_format($this->arquivo->getValorTotalEmTitulos(), 2));
+    }
 
 }
